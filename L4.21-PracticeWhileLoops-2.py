633470df1c17dba8f10251e86b6982101f879095
Etlarose <67241280+Etlarose@users.noreply.github.com>
2021-07-31 13:03:52 -0600

    Create L4.21-PracticeWhileLoops-2.py

---
+++ L4.21-PracticeWhileLoops-2.py
@@ -0,0 +1,12 @@
+# number to find the factorial of
+number = 6   
+
+# start with our product equal to one
+product = 1
+
+# write your for loop here
+
+
+
+# print the factorial of number
+print(product)
