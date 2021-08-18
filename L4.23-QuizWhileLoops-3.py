e8c29f005e4f6f69b07fc71ea5ac931b86f34155
Etlarose <67241280+Etlarose@users.noreply.github.com>
2021-08-17 20:19:27 -0600

    Update L4.23-QuizWhileLoops-3.py

--- L4.23-QuizWhileLoops-3.py
+++ L4.23-QuizWhileLoops-3.py
@@ -1,17 +1,8 @@
-limit = 50
-nearest_square = 0
-x = 1
-break_num = 0
-# write your while loop here
-while break_num < limit:
-    break_num = x * x
-    if break_num > limit:
-        #print(nearest_square)
-        break
-    # print(nearest_square)
-    else:
-        nearest_square = x * x
-        x += 1
-        #print(x)
+limit = 40
+
+num = 0
+while (num+1)**2 < limit:
+    num += 1
+nearest_square = num**2
 
 print(nearest_square)
