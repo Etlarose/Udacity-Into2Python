029504607ebd49f2f1bb0cab2ebd14d475cc3708
Etlarose <67241280+Etlarose@users.noreply.github.com>
2021-08-17 19:45:55 -0600

    Update L4.23-QuizWhileLoops-2.py

--- L4.23-QuizWhileLoops-2.py
+++ L4.23-QuizWhileLoops-2.py
@@ -1,19 +1,15 @@
-start_num = 5 #provide some start number
-end_num = 11 #provide some end number that you stop when you hit
-count_by = 1#provide some number to count by 
-break_num = 0
-# write a condition to check that end_num is larger than start_num before looping
+start_num = 5
+end_num = 100
+count_by = 2
+
 if start_num > end_num:
-    result = "Oops! Looks like your start valye is greater than the end value. Please try again"
-    print(result)
-    exit()
-    
-# write a while loop that uses break_num as the ongoing number to 
-#   check against end_num
-while break_num <= end_num:
-        # sanity check print("Break ", break_num)
-        # sanity check print("Start ", start_num)
+    result = "Oops! Looks like your start value is greater than the end value. Please try again."
+
+else:
+    break_num = start_num
+    while break_num < end_num:
         break_num += count_by
-        result = break_num
+
+    result = break_num
 
 print(result)
