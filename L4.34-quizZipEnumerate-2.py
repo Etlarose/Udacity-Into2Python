93116108aaf73bee73cc8e7484cdae8d7d6dac6b
Etlarose <67241280+Etlarose@users.noreply.github.com>
2021-09-07 09:57:39 -0600

    Create L4.34-quizZipEnumerate-2.py

---
+++ L4.34-quizZipEnumerate-2.py
@@ -0,0 +1,10 @@
+cast_names = ["Barney", "Robin", "Ted", "Lily", "Marshall"]
+cast_heights = [72, 68, 72, 66, 76]
+
+
+cast = {}
+
+for name, height in zip(cast_names, cast_heights):
+    cast[name] = height    
+    
+print(cast)
