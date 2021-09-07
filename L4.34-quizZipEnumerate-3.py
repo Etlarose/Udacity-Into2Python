338c2ed1e32ea157474cd03284a7958426ac819d
Etlarose <67241280+Etlarose@users.noreply.github.com>
2021-09-07 10:25:50 -0600

    Update L4.34-quizZipEnumerate-3.py

--- L4.34-quizZipEnumerate-3.py
+++ L4.34-quizZipEnumerate-3.py
@@ -1,12 +1,5 @@
 cast = (("Barney", 72), ("Robin", 68), ("Ted", 72), ("Lily", 66), ("Marshall", 76))
 
-#define names and heights here
-names = []
-heights = []
-
-for i in range(len(cast)):
-    names, heights = zip(*cast)
-
-
+names, heights = zip(*cast)
 print(names)
 print(heights)
