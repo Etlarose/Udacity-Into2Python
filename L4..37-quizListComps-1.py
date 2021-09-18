a42a800e404aac799cee19d6649afb7813df4b5d
Etlarose <67241280+Etlarose@users.noreply.github.com>
2021-09-18 12:41:29 -0600

    Update L4..37-quizListComps-1.py

--- L4..37-quizListComps-1.py
+++ L4..37-quizListComps-1.py
@@ -1,5 +1,4 @@
 names = ["Rick Sanchez", "Morty Smith", "Summer Smith", "Jerry Smith", "Beth Smith"]
 
-first_names = [name.lower().split()[0] for name in names] # write your list comprehension here
-
+first_names = [name.split()[0].lower() for name in names]
 print(first_names)
